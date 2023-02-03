120516b530337cef745dd56edd48df3c4a50e14b
omar <omarkorety@hotmail.com>
2023-02-03 16:01:24 +0200

    changing

--- Terraform/templates/ubuntu.sh
+++ Terraform/templates/ubuntu.sh
@@ -9,5 +9,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
-sudo docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name=jenkins_master --restart=always  jenkins/jenkins
+sudo docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name=jenkins_master --restart=always  omarkorety/myjenkins:V7
+
 
